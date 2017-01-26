67875899d3caf7c359de6f718a4c90cca9e587ef
Jaime Machado <jaime.mchado@gmail.com>
2017-01-26 21:27:47 +0100

    Fixing path for linux

--- Library/GoogleMaps/__init__.py
+++ Library/GoogleMaps/__init__.py
@@ -3,7 +3,7 @@ from os import path
 from app import app
 
 def initGoogle(app):
-    googlePath = path.dirname(path.realpath(__file__))+ "\..\..\..\googleKey.txt";
+    googlePath = path.dirname(path.realpath(__file__))+ "/../../../googleKey.txt";
     with open(googlePath) as f: s = f.read()
     app.config['GOOGLE_KEY'] = s;
 
